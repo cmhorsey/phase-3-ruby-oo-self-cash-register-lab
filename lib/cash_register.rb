3e602403fef0b05c37c71d1bd679fc38760e3982
Cayla Horsey <cayla.horsey@powerhrg.com>
2024-07-11 11:33:41 -0400

    Pass all but last test

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,33 @@
+class CashRegister
+  attr_accessor :discount, :total, :items
+  attr_reader :price
 
+  def initialize(discount = nil, total = 0, items = [] )
+    @discount = discount
+    @total = total
+    @items = items
+  end
+
+  def price
+    @price = price
+  end
+
+  def add_item(title, price, quantity = 1)
+    @total += price * quantity
+    quantity.times { @items << title }
+
+  end
+
+  def apply_discount
+    if @discount == nil
+      "There is no discount to apply."
+    else
+      @total -= (@total * @discount / 100.0)
+      "After the discount, the total comes to $#{@total.to_i}."
+    end
+  end
+
+  def void_last_transaction
+
+  end
+end
